6124b86e738d98c43d2fc4a4575b27ee558982bd
Welf9i <welfkimedamine@gmail.com>
2024-12-11 18:11:54 +0100

    optimizing Vite for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  // optimizing Vite for production
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://devproject-hub.onrender.com/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
